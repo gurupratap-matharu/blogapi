ffb64071b4037a6184f79b24ed9e7373b2320c75
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-02 18:34:38 -0300

    upgraded permissions at object level

--- posts/permissions.py
+++ posts/permissions.py
@@ -2,6 +2,7 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
+
     def has_object_permission(self, request, view, obj):
         # Read only is allowed for any request
         if request.method in permissions.SAFE_METHODS:
