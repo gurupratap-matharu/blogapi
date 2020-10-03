a749cc80cfb03993ae0dc467ea0494aa5df864b4
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-03 10:43:09 -0300

    added endpoint for docs

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.documentation import include_docs_urls
 from rest_framework.schemas import get_schema_view
 
 schema_view = get_schema_view(title='Blog API')
@@ -25,5 +26,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/rest-auth/', include('rest_auth.urls')),
     path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('docs/', include_docs_urls(title='Blog API')),
     path('schema/', schema_view),
 ]
